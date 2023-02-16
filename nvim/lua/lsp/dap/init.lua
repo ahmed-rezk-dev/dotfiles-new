0484aed277aa5065a36dd733d7dfccefad45d013
Ahmed Rezk <work72019@gmail.com>
2023-02-15 18:56:03 -0700

    refactoring neovim dap

--- nvim/lua/lsp/dap/init.lua
+++ nvim/lua/lsp/dap/init.lua
@@ -1,15 +1,28 @@
 local M = {}
 
 M.setup = function()
-    -- telescope-dap
-    require("telescope").load_extension "dap"
+    local dap_status_ok, dap = pcall(require, "dap")
+    if not dap_status_ok then
+        return
+    end
 
-    -- nvim-dap-virtual-text. Show virtual text for current frame
-    vim.g.dap_virtual_text = true
+    local dap_ui_status_ok, dapui = pcall(require, "dapui")
+    if not dap_ui_status_ok then
+        return
+    end
 
-    -- nvim-dap-ui
-    require("dapui").setup {
-        icons = { expanded = "▾", collapsed = "▸" },
+    local dap_install_status_ok, dap_install = pcall(require, "dap-install")
+    if not dap_install_status_ok then
+        return
+    end
+
+    dap_install.setup {}
+
+    dap_install.config("python", {})
+    -- add other configs here
+
+    dapui.setup {
+        icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
         mappings = {
             -- Use a table to apply multiple mappings
             expand = { "<CR>", "<2-LeftMouse>" },
@@ -33,21 +46,37 @@ M.setup = function()
             {
                 elements = {
                     -- Elements can be strings or table with id and size keys.
-                    { id = "scopes", size = 0.45 },
+                    { id = "scopes", size = 0.25 },
                     "breakpoints",
                     "stacks",
                     "watches",
                 },
-                size = 80, -- 40 columns
+                size = 40, -- 40 columns
                 position = "right",
             },
             {
                 elements = {
-                    -- "repl",
-                    -- "console",
+                    "repl",
+                    "console",
                 },
-                size = 0.15, -- 25% of total lines
+                size = 0.25, -- 25% of total lines
                 position = "bottom",
+            },
+        },
+        controls = {
+            -- Requires Neovim nightly (or 0.8 when released)
+            enabled = true,
+            -- Display controls in this element
+            element = "repl",
+            icons = {
+                pause = "",
+                play = "",
+                step_into = "",
+                step_over = "",
+                step_out = "",
+                step_back = "",
+                run_last = "↻",
+                terminate = "□",
             },
         },
         floating = {
@@ -61,83 +90,26 @@ M.setup = function()
         windows = { indent = 1 },
         render = {
             max_type_length = nil, -- Can be integer or nil.
+            max_value_lines = 100, -- Can be integer or nil.
         },
     }
-    -- local dap_install = require "dap-install"
-    -- dap_install.config("chrome", {})
 
-    -- languages
-    -- require('dbg.python')
-    -- require('dbg.node')
-    require "lsp.dap.react"
+    vim.fn.sign_define("DapBreakpoint", { text = "⬤", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapStopped", { text = "⏭", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 
-    -- nvim-dap
-    vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "ErrorMsg", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapStopped", { text = "●▶", texthl = "InfoMsg", linehl = "MatchParen", numhl = "" })
-    vim.fn.sign_define("DapBreakpointCondition", { text = "C", texthl = "", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapBreakpointRejected", { text = "●", texthl = "WarningMsg", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapLogPoint", { text = "L", texthl = "", linehl = "", numhl = "" })
-
-    require("nvim-dap-virtual-text").setup {
-        enabled = true, -- enable this plugin (the default)
-        enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-        show_stop_reason = true, -- show stop reason when stopped for exceptions
-        commented = false, -- prefix virtual text with comment string
-        -- experimental features:
-        virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
-        all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-        virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-        -- virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-        -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-    }
-
-    local function map(mode, lhs, rhs, opts)
-        local options = { noremap = true }
-        if opts then
-            options = vim.tbl_extend("force", options, opts)
-        end
-        vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
     end
 
-    -- -- key mappings
-    -- utils.map('n', '<leader>dct', '<cmd>lua require"dap".continue()<CR>')
-    -- utils.map('n', '<leader>dsv', '<cmd>lua require"dap".step_over()<CR>')
-    -- utils.map('n', '<leader>dsi', '<cmd>lua require"dap".step_into()<CR>')
-    -- utils.map('n', '<leader>dso', '<cmd>lua require"dap".step_out()<CR>')
-    -- utils.map('n', '<leader>dtb', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
-    --
-    -- map("n", "<leader>dsk", '<cmd>lua require"dap.ui.variable".scopes()<CR>')
-    -- map("n", "<leader>dhk", '<cmd>lua require"dap.ui.variables".hover()<CR>')
-    -- utils.map('v', '<leader>dhv',
-    --           '<cmd>lua require"dap.ui.variables".visual_hover()<CR>')
-    --
-    -- utils.map('n', '<leader>duh', '<cmd>lua require"dap.ui.widgets".hover()<CR>')
-    -- utils.map('n', '<leader>duf',
-    --           "<cmd>lua local widgets=require'dap.ui.widgets';widgets.centered_float(widgets.scopes)<CR>")
-    --
-    -- utils.map('n', '<leader>dsbr',
-    --           '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>')
-    -- utils.map('n', '<leader>dsbm',
-    --           '<cmd>lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>')
-    -- utils.map('n', '<leader>dro', '<cmd>lua require"dap".repl.open()<CR>')
-    -- utils.map('n', '<leader>drl', '<cmd>lua require"dap".repl.run_last()<CR>')
-    --
-    -- -- telescope-dap
-    -- utils.map('n', '<leader>dcc',
-    --           '<cmd>lua require"telescope".extensions.dap.commands{}<CR>')
-    -- utils.map('n', '<leader>dco',
-    --           '<cmd>lua require"telescope".extensions.dap.configurations{}<CR>')
-    -- utils.map('n', '<leader>dlb',
-    --           '<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>')
-    -- utils.map('n', '<leader>dv',
-    --           '<cmd>lua require"telescope".extensions.dap.variables{}<CR>')
-    -- utils.map('n', '<leader>df',
-    --           '<cmd>lua require"telescope".extensions.dap.frames{}<CR>')
-    --
-    -- -- nvim-dap-ui
-    -- utils.map('n', '<leader>dui', '<cmd>lua require"dapui".toggle()<CR>')s
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+    end
+
+    dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+    end
+
+    require("lsp.dap.javascript").setup()
 end
 
 return M
---
+++ nvim/lua/lsp/dap/javascript.lua
@@ -0,0 +1,80 @@
+--[[ local DEBUGGER_PATH = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug" ]]
+local DEBUGGER_PATH = vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter"
+
+local M = {}
+
+function M.setup()
+    require("dap-vscode-js").setup {
+        node_path = "node",
+        debugger_path = DEBUGGER_PATH,
+        -- debugger_cmd = { "js-debug-adapter" },
+        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+    }
+
+    for _, language in ipairs { "typescript", "javascript" } do
+        require("dap").configurations[language] = {
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require("dap.utils").pick_process,
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Debug Jest Tests",
+                -- trace = true, -- include debugger info
+                runtimeExecutable = "node",
+                runtimeArgs = {
+                    "./node_modules/jest/bin/jest.js",
+                    "--runInBand",
+                },
+                rootPath = "${workspaceFolder}",
+                cwd = "${workspaceFolder}",
+                console = "integratedTerminal",
+                internalConsoleOptions = "neverOpen",
+            },
+        }
+
+        for _, language in ipairs { "typescriptreact", "javascriptreact" } do
+            require("dap").configurations[language] = {
+                {
+                    type = "pwa-chrome",
+                    name = "Attach - Remote Debugging",
+                    request = "attach",
+                    program = "${file}",
+                    cwd = vim.fn.getcwd(),
+                    sourceMaps = true,
+                    protocol = "inspector",
+                    port = 9222,
+                    webRoot = "${workspaceFolder}",
+                    processId = require("dap.utils").pick_process,
+                },
+                {
+
+                    program = "${file}",
+                    cwd = vim.fn.getcwd(),
+                    sourceMaps = true,
+                    protocol = "inspector",
+                    port = 9222,
+                    type = "pwa-chrome",
+                    name = "Launch Chrome",
+                    request = "launch",
+                    host = "https://localhost:5001",
+                    webRoot = "${workspaceFolder}/src",
+                    url = "https://localhost:5001",
+                },
+            }
+        end
+    end
+end
+
+return M
