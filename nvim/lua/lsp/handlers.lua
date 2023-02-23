6cfe2a7c155d8bcd8e9f0d90a094043776c8faec
Ahmed Rezk <work72019@gmail.com>
2023-02-22 20:12:36 -0700

    fix diagnostic configuration

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -5,7 +5,7 @@ local lsp_keymaps = require "lsp.lsp-keymaps"
 
 local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_cmp_ok then
-  return
+    return
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -13,32 +13,32 @@ M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
-  vim.diagnostic.config(diagnostic_config)
+    vim.diagnostic.config(diagnostic_config.setup())
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+    })
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-  })
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+    })
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
-    client.server_capabilities.documentFormattingProvider = false
-  end
+    if client.name == "tsserver" then
+        client.server_capabilities.documentFormattingProvider = false
+    end
 
-  if client.name == "sumneko_lua" then
-    client.server_capabilities.documentFormattingProvider = false
-  end
+    if client.name == "sumneko_lua" then
+        client.server_capabilities.documentFormattingProvider = false
+    end
 
-  lsp_keymaps.setup(bufnr)
-  local status_ok, illuminate = pcall(require, "illuminate")
-  if not status_ok then
-    return
-  end
-  illuminate.on_attach(client)
+    lsp_keymaps.setup(bufnr)
+    local status_ok, illuminate = pcall(require, "illuminate")
+    if not status_ok then
+        return
+    end
+    illuminate.on_attach(client)
 end
 
 return M
